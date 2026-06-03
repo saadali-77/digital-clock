3a21db46c8a074c93efd2d1a05f372afe0bcebc6
saadali <saadali6991550@gmail.com>
2026-06-04 01:12:12 +0500

    initial

---
+++ src/DigitalClock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const DigitalClock = () => {
+  return (
+    <div className='clock-container'>
+        <div className='clock'>
+            <span>00:00:00</span>
+
+        </div>
+        
+        
+        </div>
+  )
+}
